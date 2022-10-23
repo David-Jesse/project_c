978f8a4799d7620ce22914650581713eddc2b964
David-Jesse <djesse351@gmail.com>
2022-10-22 22:17:41 -0700

    More tasks on c

---
+++ applying_pointers/task3.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/* function declaration */
+void StrPrint(char str[][15], int max);
+/* main function */
+int main()
+{
+	char str[2][15] = {"You know what,", 
+		"C is powerful."};
+
+	StrPrint(str, 2);
+	
+	return 0;
+}
+void StrPrint(char str[][15], int max)
+{
+	int i;
+
+	for (i=0; i<max; i++)
+		printf("%s\n", str[i]);
+}
+
+
